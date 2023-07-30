e8bfde22bc9f210f10a05448e3b99fdce9510d1f
Nor7ex <timking2580@gmail.com>
2023-07-31 02:21:12 +0300

    add deals and adaptive it

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector("#burger");
 
-burger.addEventListener("click", () => {
+burger.addEventListener("click", () =>{
   burger.classList.toggle("menu-open");
   document.body.classList.toggle("menu-open");
   if(burger.classList.contains("menu-open")) document.body.setAttribute("data-scroll", "false");
@@ -15,4 +15,14 @@ const swiper = new Swiper(".slider-main-block", {
 		nextEl: ".body-main-block__arrow.swiper-button-next",
 		prevEl: ".body-main-block__arrow.swiper-button-prev",
 	},
+});
+
+// Tabs
+const tabNavItems =document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll(".item-tabs");
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	if(targetElement.closest('.tabs-deals__button')) {
+		// Дороблю завтра
+	}
 });
