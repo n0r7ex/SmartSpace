42a8d6456e7f9fc8795b4e55984ee964f107b8c2
Nor7ex <timking2580@gmail.com>
2023-07-31 13:42:11 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -22,7 +22,20 @@ const tabNavItems =document.querySelectorAll('.tabs-deals__button');
 const tabItems = document.querySelectorAll(".item-tabs");
 document.addEventListener("click", function (e) {
 	const targetElement = e.target;
+	let currentActiveIndex = null;
+	let newActiveIndex = null;
 	if(targetElement.closest('.tabs-deals__button')) {
-		// Дороблю завтра
+		tabNavItems.forEach((tabNavItem, index)  => {
+			if (tabNavItem.classList.contains('active')){
+				currentActiveIndex = index;
+				tabNavItem.classList.remove("active");
+			}
+			if (tabNavItem === targetElement){
+				newActiveIndex = index;
+			}
+		});
+		targetElement.classList.add("active");
+		tabItems[currentActiveIndex].classList.remove("active");
+		tabItems[newActiveIndex].classList.add("active");
 	}
 });
